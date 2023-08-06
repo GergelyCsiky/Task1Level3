e0959d1b00308600c9f8eafc3877c0723023449b
Gergely <g.csiky@gmail.com>
2023-08-06 08:55:10 +0200

    Added another print message

--- git_task_project/helloWorld.py
+++ git_task_project/helloWorld.py
@@ -1,3 +1,7 @@
 #print("Git is awsome")
 user_input = input("Waht would you like me to print?: ")
 print(user_input)
+# Changing print message
+
+user_input2 = input("Anything else you want me to prit?")
+print(user_input2)
